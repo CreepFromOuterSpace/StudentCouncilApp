005fd0ae902704eb21db4719dfc98cac66bc3868
CreepFromOuterSpace <104240259+CreepFromOuterSpace@users.noreply.github.com>
2022-12-25 10:40:39 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,7 @@ import streamlit as st
 
 username = st.text_input('Username')
 password = st.text_input('Password', type="password")
-st.button('Register')
+
+if st.button('Register')
+  with st.spinner(text="Registering user...")
+    pass
