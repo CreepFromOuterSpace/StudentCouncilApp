9f6356eb52bb2d0cff1a4d010bebe0bbe86366aa
CreepFromOuterSpace <104240259+CreepFromOuterSpace@users.noreply.github.com>
2022-12-25 10:43:50 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,14 @@
 import streamlit as st
 
-username = st.text_input('Username')
+username = st.text_input('Email (Use Your ***********@bishopbrady.edu Email)')
 password = st.text_input('Password', type="password")
 
-if st.button('Register')
+if st.button('Register'):
   with st.spinner(text="Registering user...")
-    pass
+    with open(r'users.txt', 'r') as fp:
+    lines = fp.readlines()
+    for row in lines:
+        word = password
+        if row.find(word) != -1:
+            print('User already exists.')
+            print('line Number:', lines.index(line))
