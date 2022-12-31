1051b3d4c78a81ce2a18c452395d46998946b88e
CreepFromOuterSpace <104240259+CreepFromOuterSpace@users.noreply.github.com>
2022-12-31 11:00:09 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ with col1:
   username = st.text_input('Email (Use Your ***********@bishopbrady.edu Email)')
   password = st.text_input('Password', type="password")
 
-  for st.button('Register'):
+  if st.button('Register'):
    with st.spinner(text="Registering user..."):
      if (username in user_list):
        st.warning('''There's already an account with that email.''')
