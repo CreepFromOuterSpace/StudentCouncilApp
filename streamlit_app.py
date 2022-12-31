e7f73caffb05c81351b16804bcacf79c83459853
CreepFromOuterSpace <104240259+CreepFromOuterSpace@users.noreply.github.com>
2022-12-31 10:48:37 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,3 +20,12 @@ with col1:
 with col2:
   login_user = st.text_imput('Email')
   login_pass = st.text_input('Password', type="password")
+  
+  if st.button('Login')
+    with st.spinner(text="Logging in..."):
+      if (login_user in user_list):
+        index = user_list.index(login_user)
+        if pass_list[index] = login_pass
+          st.success('Logged in!')
+      else:
+        st.warning('''I couldn't find an account with that email. Please register first.''')
